fe0a41a94e718e0dbb51169c37d13961a2c53ba4
unknown <tohami00076@gmail.com>
2022-08-29 05:31:08 -0700

    finsh

---
+++ database/seeds/Moshref_table_seeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Moshref_table_seeder extends Seeder
+{
+
+    public function run()
+    {
+        $user = \App\User::create([
+            'first_name' => 'El ',
+            'last_name' => 'Moshref',
+            'email' => 'toohami09952@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $user->attachRole('moshref');
+    }
+}
